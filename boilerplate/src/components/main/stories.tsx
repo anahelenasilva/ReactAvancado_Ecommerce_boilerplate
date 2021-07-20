98a7c73a8c34a80aaba3fc3ce39f31512a7598e7
Ana Helena <anahelenarp@hotmail.com>
2021-07-20 19:27:45 -0300

    Migração storybook v6

--- boilerplate/src/components/main/stories.tsx
+++ boilerplate/src/components/main/stories.tsx
@@ -1,4 +1,3 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import { Story, Meta } from '@storybook/react'
 
 import Main from '.'
@@ -6,15 +5,10 @@ import Main from '.'
 export default {
   title: 'Main',
   component: Main,
-  decorators: [withKnobs]
+  args: {
+    title: 'title default',
+    description: 'description default'
+  }
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
